f93614743a122351840f76e8fb9c94816a5411f5
chenxyzy <1373848088@qq.com>
2015-10-27 13:29:40 +0800

    delete

--- typo3_src/typo3/sysext/css_styled_content/Configuration/TCA/Overrides/pages.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('TYPO3_MODE') or die();
-
-// Add pageTSconfig
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
-	'css_styled_content',
-	'Configuration/PageTSconfig/NewContentElementWizard.ts',
-	'CSS-based Content Elements'
-);
